f12e3827d71c6cca11976c21671a6d0932b9b8d2
CHRISSS IANNN <christian.aldo947@gmail.com>
2026-03-12 01:54:28 +0700

    Update bot_tele.py

--- bot_tele.py
+++ bot_tele.py
@@ -473,23 +473,26 @@ def main():
 
     app.run_polling()
 
-import threading
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot is running")
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(("0.0.0.0", port), Handler)
-    server.serve_forever()
-
-threading.Thread(target=run_web).start()
-
 if __name__ == "__main__":
+
+    import threading
+    from http.server import HTTPServer, BaseHTTPRequestHandler
+
+    class Handler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Bot is running")
+
+    def run_web():
+        port = int(os.environ.get("PORT", 10000))
+        server = HTTPServer(("0.0.0.0", port), Handler)
+        server.serve_forever()
+
+    threading.Thread(target=run_web).start()
+
     main()
+  
+
 
 
